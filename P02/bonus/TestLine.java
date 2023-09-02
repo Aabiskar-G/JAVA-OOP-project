bbea08da1925da494859160aac9f18b5644b35a9
axg0946 <axg0946@mavs.uta.edu>
2023-09-02 04:45:56 -0500

    second assignment

--- P02/bonus/TestLine.java
+++ P02/bonus/TestLine.java
@@ -1,7 +1,7 @@
 public class TestLine{
 	public static void main(String[] args) {
 		Line line1 = new Line(1.23, 2.23, 3.34, 4.34, Color.PURPLE);
-		Line line2 = new Line(2.23, 3.23, 5.34, 6.34, Color.BLACK;
+		Line line2 = new Line(2.23, 3.23, 5.34, 6.34, Color.BLACK);
 		Line line3 = new Line(0.00, 0.00, 7.34, 8.34, Color.WHITE);
 		Line line4 = new Line(4.23, 5.23, 7.34, 9.34, Color.CYAN);
 		
