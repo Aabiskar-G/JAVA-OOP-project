2e9e849186611d8014fed5a2196d9d193163b5f4
Aabiskar-G <143457604+Aabiskar-G@users.noreply.github.com>
2023-10-03 05:41:47 -0500

    Delete P06/hsdg directory

--- P06/hsdg/docs/api/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"library","l":"Video.InvalidRuntime"},{"p":"library","l":"Library"},{"p":"library","l":"Patron"},{"p":"library","l":"Publication"},{"p":"library","l":"Video"}]
