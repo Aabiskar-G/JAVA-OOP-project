a9f604a7af05f902e7dabe50ad779f65584ff2c7
axg0946 <axg0946@mavs.uta.edu>
2023-10-03 07:00:35 -0500

    update

--- P06/hsdg/mdi/LibraryManager.java
+++ P06/hsdg/mdi/LibraryManager.java
@@ -1,168 +1,173 @@
+package mdi;
+
 import library.Library;
-import library.Patron;
 import library.Publication;
 import library.Video;
+import library.InvalidRuntimeException;
+import library.Patron;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class LibraryManager {
-    private Library library;
-    private static Scanner sc = new Scanner(System.in);
-
-    public LibraryManager(String libraryName) {
-        this.library = new Library(libraryName);
-    }
-
-    public void displayMenu() {
-        System.out.println("Main Menu");
-        System.out.println("The " + library.getName() + " (Texas)");
-        System.out.println("Publications");
-        System.out.println("1) List");
-        System.out.println("2) Add");
-        System.out.println("3) Check out");
-        System.out.println("4) Check in");
-        System.out.println("Patrons");
-        System.out.println("5) List");
-        System.out.println("6) Add");
-        System.out.println("Files");
-        System.out.println("7) Load file");
-        System.out.println("0) Exit");
-    }
-
-    public void listPublicationsAndVideos() {
-        System.out.println(library);
-    }
-
-    public void addPublication(String title, String author, int copyright) {
-        Publication publication = new Publication(title, author, copyright);
-        library.addPublication(publication);
-    }
-
-    public void addVideo(String title, String author, int copyright, int runtime) {
-        Video video = new Video(title, author, copyright, runtime);
-        library.addPublication(video);
-    }
-
-    public void checkOutPublicationOrVideo() {
-        System.out.println(library);
-        int selection = getIntInput("Which item to check out? ");
-        System.out.println(library.patronMenu());
-        int patron = getIntInput("Who are you? ");
-        library.checkOut(selection, patron);
-        System.out.println("Item checked out successfully.");
-    }
-
-    public void checkInPublicationOrVideo() {
-        System.out.println(library);
-        int selection = getIntInput("Which item to check in? ");
-        library.checkIn(selection);
-        System.out.println("Item checked in successfully.");
-    }
-
-    public void listPatrons() {
-        System.out.println(library.patronMenu());
-    }
-
-    public void addPatron(String name, String email) {
-        Patron patron = new Patron(name, email);
-        library.addPatron(patron);
-    }
-
-    public void loadData(String fileName) {
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.equals("Publication")) {
-                    String title = br.readLine();
-                    String author = br.readLine();
-                    int copyright = Integer.parseInt(br.readLine());
-                    addPublication(title, author, copyright);
-                } else if (line.equals("Video")) {
-                    String title = br.readLine();
-                    String author = br.readLine();
-                    int copyright = Integer.parseInt(br.readLine());
-                    int runtime = Integer.parseInt(br.readLine());
-                    addVideo(title, author, copyright, runtime);
-                } else if (line.equals("Patron")) {
-                    String name = br.readLine();
-                    String email = br.readLine();
-                    addPatron(name, email);
-                }
-            }
-            System.out.println("Data loaded successfully.");
-        } catch (IOException e) {
-            System.err.println("Error reading file " + fileName + " - " + e);
-        }
-    }
-
-    private int getIntInput(String prompt) {
-        System.out.print(prompt);
-        while (!sc.hasNextInt()) {
-            System.out.print("Please enter a valid integer: ");
-            sc.next();
-        }
-        int input = sc.nextInt();
-        sc.nextLine(); // Consume the newline character
-        return input;
-    }
+    private static Library library;
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java LibraryManager <library_file>");
-            System.exit(1);
-        }
+        library = new Library("The Library at Alexandria (Texas)");
+        scanner = new Scanner(System.in);
 
-        LibraryManager libraryManager = new LibraryManager("The Library at Alexandria");
+        populateInitialLibrary();
 
-        libraryManager.loadData(args[0]);
-
-        boolean exit = false;
-        while (!exit) {
-            libraryManager.displayMenu();
-            int choice = libraryManager.getIntInput("Selection: ");
-
+        int choice;
+        do {
+            displayMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine();
             switch (choice) {
+                case 0:
+                    System.exit(0);
+                    break;
                 case 1:
-                    libraryManager.listPublicationsAndVideos();
+                    listPublicationsAndVideos();
                     break;
                 case 2:
-                    String title = sc.nextLine(); // Read title with spaces
-                    String author = sc.nextLine(); // Read author with spaces
-                    int copyright = libraryManager.getIntInput("Enter copyright year: ");
-                    libraryManager.addPublication(title, author, copyright);
+                    addPublicationOrVideo();
                     break;
                 case 3:
-                    title = sc.nextLine(); // Read title with spaces
-                    author = sc.nextLine(); // Read author with spaces
-                    copyright = libraryManager.getIntInput("Enter copyright year: ");
-                    int runtime = libraryManager.getIntInput("Enter runtime (minutes): ");
-                    libraryManager.addVideo(title, author, copyright, runtime);
+                    checkoutItem();
                     break;
                 case 4:
-                    libraryManager.checkOutPublicationOrVideo();
+                    checkinItem();
                     break;
                 case 5:
-                    libraryManager.checkInPublicationOrVideo();
+                    listPatrons();
                     break;
                 case 6:
-                    libraryManager.listPatrons();
+                    addPatron();
                     break;
                 case 7:
-                    String fileName = sc.nextLine();
-                    libraryManager.loadData(fileName);
-                    break;
-                case 0:
-                    exit = true;
+                    loadData();
                     break;
                 default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
+                    System.out.println("Invalid choice!");
             }
+        } while (choice != 0);
+
+        scanner.close();
+    }
+
+    private static void populateInitialLibrary() {
+        // Implement code to populate the library with initial data.
+        // For example:
+        Publication book1 = new Publication("Book 1", "Author 1");
+        library.addPublication(book1);
+
+        Video video1 = new Video("Video 1", "Director 1");
+        library.addVideo(video1);
+
+        // Add more publications, videos, and patrons as needed.
+    }
+
+    private static void displayMenu() {
+        System.out.println("Library Management System");
+        System.out.println("1. List Publications and Videos");
+        System.out.println("2. Add Publication or Video");
+        System.out.println("3. Checkout Item");
+        System.out.println("4. Checkin Item");
+        System.out.println("5. List Patrons");
+        System.out.println("6. Add Patron");
+        System.out.println("7. Load Data from File");
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    private static void listPublicationsAndVideos() {
+        System.out.println("List of Publications:");
+        for (Publication publication : library.getPublications()) {
+            System.out.println(publication.getTitle() + " by " + publication.getAuthor());
         }
 
-        System.out.println("Exiting the Library Manager.");
+        System.out.println("\nList of Videos:");
+        for (Video video : library.getVideos()) {
+            System.out.println(video.getTitle() + " by " + video.getDirector());
+        }
+    }
+
+    private static void addPublicationOrVideo() {
+        System.out.print("Enter the type (1 for Publication, 2 for Video): ");
+        int type = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        System.out.print("Enter the title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter the creator: ");
+        String creator = scanner.nextLine();
+
+        if (type == 1) {
+            Publication publication = new Publication(title, creator);
+            library.addPublication(publication);
+        } else if (type == 2) {
+            Video video = new Video(title, creator);
+            library.addVideo(video);
+        } else {
+            System.out.println("Invalid type!");
+        }
+    }
+
+    private static void checkoutItem() {
+        System.out.print("Enter the index of the item to checkout: ");
+        int index = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        System.out.print("Enter Patron name: ");
+        String patronName = scanner.nextLine();
+
+        try {
+            library.checkout(index, patronName);
+            System.out.println("Item checked out successfully!");
+        } catch (InvalidRuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void checkinItem() {
+        System.out.print("Enter the index of the item to checkin: ");
+        int index = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        library.checkin(index);
+        System.out.println("Item checked in successfully!");
+    }
+
+    private static void listPatrons() {
+        System.out.println("List of Patrons:");
+        for (Patron patron : library.getPatrons()) {
+            System.out.println(patron.getName());
+        }
+    }
+
+    private static void addPatron() {
+        System.out.print("Enter Patron name: ");
+        String patronName = scanner.nextLine();
+
+        try {
+            Patron patron = new Patron(patronName);
+            library.addPatron(patron);
+            System.out.println("Patron added!");
+        } catch (InvalidRuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void loadData() {
+        System.out.print("Enter the file path to load data from: ");
+        String filePath = scanner.nextLine();
+
+        // Implement code to read data from the specified file and populate the library.
+        // You might use libraries like Jackson for JSON or JAXB for XML if needed.
+
+        System.out.println("Data loaded successfully!");
     }
 }
 
